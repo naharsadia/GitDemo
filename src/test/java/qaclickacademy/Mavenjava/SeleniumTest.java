4c5bc400e4f7586a27ec04355601a07326430db9
naharsadia <nahar_sadia@yahoo.com>
2020-08-11 14:30:29 -0400

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,25 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementsUI()
+	{
+		System.out.println("ElementsUI");
+	}
+	
+	
+}
+
+//C:\Users\PSQA\Documents\EclipseWorkSpace\Mavenjava
+//mvn -Dtest=TestCircle test <==> running a single test, providing test name in the command prompt 
