088111d7c903daace00b423b16703fa985725ddd
naharsadia <nahar_sadia@yahoo.com>
2020-08-13 16:50:25 -0400

    commit from america

--- src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -7,11 +7,14 @@ public class RESTAPITest {
 	@Test
 	public void PostJira()
 	{
-		System.out.println("PostJira");
+		System.out.println("PostJira1");
 		System.out.println("PostJira2");
 		System.out.println("PostJira3");
 		System.out.println("PostJira4");
-		
+		System.out.println("PostJira5");
+		System.out.println("PostJira6");
+		System.out.println("PostJira7");
+		System.out.println("PostJira8");
 	}
 	
 	@Test
